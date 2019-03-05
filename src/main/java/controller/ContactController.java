e66b20a85beacef4f217f3d3b8bf170577186d20
SilvaRamon <ramonschiffer@gmail.com>
2019-03-05 15:39:22 -0300

    adicionando metodo get para buscar todos os registros da tabela

--- src/main/java/controller/ContactController.java
+++ src/main/java/controller/ContactController.java
@@ -1,8 +1,13 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rcs.model.Contact;
 
 import repository.ContactRepository;
 
@@ -13,5 +18,8 @@ public class ContactController {
 	@Autowired
 	private ContactRepository contactRepo;
 	
-	
+	@GetMapping
+	public List<Contact> findAll() {
+		return contactRepo.findAll();
+	}
 }
