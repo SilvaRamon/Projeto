4b2e16cb18c985b0189ae60e8b00540d359f1453
SilvaRamon <ramonschiffer@gmail.com>
2019-03-05 14:26:23 -0300

    adicionando controller

---
+++ src/main/java/controller/ContactController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import repository.ContactRepository;
+
+@RestController
+@RequestMapping({"/contacts"})
+public class ContactController {
+	
+	@Autowired
+	private ContactRepository contactRepo;
+	
+	
+}
