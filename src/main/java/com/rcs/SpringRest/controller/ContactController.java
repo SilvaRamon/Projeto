31ecf42f42da102085c0b955a26d720e07fe706f
SilvaRamon <ramonschiffer@gmail.com>
2019-03-06 20:52:41 -0300

    adicionando metodo para buscar contato por id

--- src/main/java/com/rcs/SpringRest/controller/ContactController.java
+++ src/main/java/com/rcs/SpringRest/controller/ContactController.java
@@ -3,11 +3,14 @@ package com.rcs.SpringRest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rcs.SpringRest.model.Contact;
@@ -20,13 +23,21 @@ public class ContactController {
 	@Autowired
 	private ContactRepository contactRepository;
 	
-	@GetMapping("/all")
+	@GetMapping
 	public List<Contact> findAll() {
 		return contactRepository.findAll();
 	}
 	
-	@PostMapping("/add")
+	@PostMapping
+	@ResponseStatus(code=HttpStatus.CREATED)
 	public Contact add(@RequestBody Contact contact) {
 		return contactRepository.save(contact);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Contact> findById(@PathVariable long id) {
+		return contactRepository.findById(id)
+				.map(result -> ResponseEntity.ok().body(result))
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
